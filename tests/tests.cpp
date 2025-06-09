00ce06abbcf94f0e20b6189c404607247d613d15
Jules Fouchy <jules.fouchy@ntymail.com>
2025-06-09 12:06:29 +0200

    🚨 Fix tests build

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -9,34 +9,39 @@ auto main(int argc, char* argv[]) -> int
     if (!should_run_imgui_tests)
         return 0;
 
-    quick_imgui::loop("ImStyleEd", [&]() { // Open a window and run all the ImGui-related code
-        static auto editor = ImStyleEd::Editor{
-            ImStyleEd::SerializationPaths{
-                .current_theme_file    = exe_path::dir() / "imstyleed_current_theme.json",
-                .themes_folder         = exe_path::dir() / "imstyleed_themes",
-                .themes_default_folder = exe_path::dir() / "imstyleed_themes",
-                .config_file           = exe_path::dir() / "imstyleed_config.json",
+    quick_imgui::loop(
+        "ImStyleEd",
+        {
+            .loop = [&]() {
+                static auto editor = ImStyleEd::Editor{
+                    ImStyleEd::SerializationPaths{
+                        .current_theme_file    = exe_path::dir() / "imstyleed_current_theme.json",
+                        .themes_folder         = exe_path::dir() / "imstyleed_themes",
+                        .themes_default_folder = exe_path::dir() / "imstyleed_themes",
+                        .config_file           = exe_path::dir() / "imstyleed_config.json",
+                    },
+                    ImStyleEd::CurrentTheme{ImStyleEd::OsThemeChecker{}},
+                    [](ImStyleEd::Config& config) {
+                        ImStyleEd::register_all_imgui_color_elements(config);
+                    }
+                };
+                ImGui::Begin("Configuration Editor");
+                {
+                    editor.imgui_config_editor();
+                }
+                ImGui::End();
+                ImGui::Begin("Themes Editor");
+                {
+                    editor.imgui_themes_editor();
+                }
+                ImGui::End();
+                ImGui::Begin("Theme Selector");
+                {
+                    editor.imgui_theme_selector();
+                }
+                ImGui::End();
+                ImGui::ShowDemoWindow();
             },
-            ImStyleEd::CurrentTheme{ImStyleEd::OsThemeChecker{}},
-            [](ImStyleEd::Config& config) {
-                ImStyleEd::register_all_imgui_color_elements(config);
-            }
-        };
-        ImGui::Begin("Configuration Editor");
-        {
-            editor.imgui_config_editor();
         }
-        ImGui::End();
-        ImGui::Begin("Themes Editor");
-        {
-            editor.imgui_themes_editor();
-        }
-        ImGui::End();
-        ImGui::Begin("Theme Selector");
-        {
-            editor.imgui_theme_selector();
-        }
-        ImGui::End();
-        ImGui::ShowDemoWindow();
-    });
+    );
 }
